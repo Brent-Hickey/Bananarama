536b21312fc3fe91329fe19ad11b51347a3e56bb
Brent Hickey <brent.hickey@icloud.com>
2023-03-05 14:07:43 -0800

    try LFE router

--- apps/bondy/src/bondy_http_gateway.erl
+++ apps/bondy/src/bondy_http_gateway.erl
@@ -926,16 +926,16 @@ base_routes() ->
 %% @doc
 %% @end
 %% -----------------------------------------------------------------------------
-admin_base_routes() ->
-    [
-        {'_', [
-            {"/ws", bondy_wamp_ws_connection_handler, #{}},
-            {"/ping", bondy_admin_ping_http_handler, #{}},
-            {"/ready", bondy_admin_ready_http_handler, #{}},
-            {"/metrics/[:registry]", prometheus_cowboy2_handler, []},
-						{"/readyLFE", bananarama_admin_ready_http_handler, #{}}
-        ]}
-    ].
+%% admin_base_routes() ->
+%%     [
+%%         {'_', [
+%%             {"/ws", bondy_wamp_ws_connection_handler, #{}},
+%%             {"/ping", bondy_admin_ping_http_handler, #{}},
+%%             {"/ready", bondy_admin_ready_http_handler, #{}},
+%%             {"/metrics/[:registry]", prometheus_cowboy2_handler, []},
+%% 						{"/readyLFE", bananarama_admin_ready_http_handler, #{}}
+%%         ]}
+%%     ].
 
 
 admin_spec() ->
